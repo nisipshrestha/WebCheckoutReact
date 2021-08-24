4a476da65042d88d6076d0e079c1d3aada9287a2
nisipshrestha <nisip_shrestha@yahoo.com>
2021-08-24 13:42:01 +0545

    Moved MerchantList to container dir

--- src/containers/WebCheckout/index.js
+++ src/containers/WebCheckout/index.js
@@ -7,7 +7,7 @@ import {
   apiSettings as settings,
   apiBaseSetter
 } from '../commonHelper';
-
+import MerchantList from '../MerchantList';
 const initialData = {
   apiKey: '3568f8c7-3f33-49dc-bbc9-9362c130f7c8',
   amount: 100.5,
@@ -28,60 +28,17 @@ function WebCheckout(props) {
   const [data, setData] = useState(initialData);
   const [env, setEnv] = useState(localStorage.getItem('env') || 'Development');
 
-  const [merchantList, setMerchantList] = useState([
-    {
-      env: 'Development',
-      name: 'Web Checkout - Development',
-      apiKey: '3568f8c7-3f33-49dc-bbc9-9362c130f7c8',
-      secretKey: '3568f8c73f3349dcbbc99362c130f7c8',
-      active: true
-    },
-    {
-      env: 'Development',
-      name: 'Western Tandoori - Development',
-      apiKey: '619cda33-b3e9-43ee-a938-c62a044fb7f2',
-      secretKey: 'cbac3523560c4f0580a29441481d785d'
-    },
-    {
-      env: 'Development',
-      name: 'Delicious Momo - Development',
-      apiKey: 'e8c961a9-3268-4ed7-939f-943835402173',
-      secretKey: 'a723551b1b004d60b43d36534d5f61bf'
-    },
-    {
-      env: 'Testing',
-      name: 'Web Checkout - Testing',
-      apiKey: '9c139d2a-0172-4e98-82f2-f4bb6d79e771',
-      secretKey: '1af9450b30b942a888859be131345c18'
-    },
-    {
-      env: 'Demo',
-      name: 'Web Checkout - Demo',
-      apiKey: 'a276f6ea-fb28-4e89-ae88-25394d7de72e',
-      secretKey: '5f8907d7b9b04a44956e8e079a362c68 '
-    },
-    {
-      env: 'Poc',
-      name: 'Web Checkout - POC',
-      apiKey: '128d26ed-4c85-4327-b439-82bbd5b2cdb8',
-      secretKey: '0e2a029569cd4347940346a0ac4a480e'
-    },
-    {
-      env: 'Vapt',
-      name: 'webcheckout merchant RH1',
-      apiKey: '50120dda-d274-4501-8800-ddee6ef07b99',
-      secretKey: '61d939510a3642cbab5f237ff92ff8a2'
-    }
-  ]);
+  const [merchantList, setMerchantList] = useState(MerchantList);
 
   const [selectedMerchantData, setSelectedMerchantData] = useState({
     name: '',
     secretKey: ''
   });
   useEffect(() => {
-    const { name, secretKey } = merchantList.find(x => x.active) || {};
+    const { name, secretKey } = merchantList.find(x => x.env === env) || {};
+    console.log([name, secretKey, env]);
     if (name && secretKey) {
-      setSelectedMerchantData({ name, secretKey });
+      setSelectedMerchantData({ name, secretKey, active: true });
     }
   }, []);
 
@@ -184,7 +141,8 @@ function WebCheckout(props) {
     ...rest
   }) => {
     settings.headers = { dvh, 'content-type': 'application/json' };
-    settings.body = JSON.stringify(rest);
+    const tempData = { ...rest };
+    settings.body = JSON.stringify(tempData);
 
     try {
       const fetchResponse = await fetch(
