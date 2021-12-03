14bca5ab7f6cba8d48a4a709830e837a173b255c
nisipshrestha <nisip_shrestha@yahoo.com>
2021-12-03 16:17:37 +0545

    Added new POC merchant TMS

--- src/containers/MerchantList.js
+++ src/containers/MerchantList.js
@@ -3,43 +3,49 @@ const merchantList = [
     env: 'Development',
     name: 'Web Checkout - Development',
     apiKey: '3568f8c7-3f33-49dc-bbc9-9362c130f7c8',
-    secretKey: '3568f8c73f3349dcbbc99362c130f7c8'
+    secretKey: '3568f8c73f3349dcbbc99362c130f7c8',
   },
   {
     env: 'Development',
     name: 'Western Tandoori - Development',
     apiKey: '619cda33-b3e9-43ee-a938-c62a044fb7f2',
-    secretKey: 'cbac3523560c4f0580a29441481d785d'
+    secretKey: 'cbac3523560c4f0580a29441481d785d',
   },
   {
     env: 'Development',
     name: 'Delicious Momo - Development',
     apiKey: 'e8c961a9-3268-4ed7-939f-943835402173',
-    secretKey: 'a723551b1b004d60b43d36534d5f61bf'
+    secretKey: 'a723551b1b004d60b43d36534d5f61bf',
   },
   {
     env: 'Testing',
     name: 'Web Checkout - Testing',
     apiKey: '9c139d2a-0172-4e98-82f2-f4bb6d79e771',
-    secretKey: '1af9450b30b942a888859be131345c18'
+    secretKey: '1af9450b30b942a888859be131345c18',
   },
   {
     env: 'Demo',
     name: 'Web Checkout - Demo',
     apiKey: 'a276f6ea-fb28-4e89-ae88-25394d7de72e',
-    secretKey: '5f8907d7b9b04a44956e8e079a362c68 '
+    secretKey: '5f8907d7b9b04a44956e8e079a362c68 ',
   },
   {
     env: 'Poc',
     name: 'Web Checkout - POC',
     apiKey: '128d26ed-4c85-4327-b439-82bbd5b2cdb8',
-    secretKey: '0e2a029569cd4347940346a0ac4a480e'
+    secretKey: '0e2a029569cd4347940346a0ac4a480e',
+  },
+  {
+    env: 'Poc',
+    name: 'TMS',
+    apiKey: '99fe577b-4f61-4e83-8327-be37ec738cfe',
+    secretKey: '14b009a00ac143efab1d3c4c7332d37a',
   },
   {
     env: 'Vapt',
     name: 'webcheckout merchant RH1',
     apiKey: '50120dda-d274-4501-8800-ddee6ef07b99',
-    secretKey: '61d939510a3642cbab5f237ff92ff8a2'
-  }
+    secretKey: '61d939510a3642cbab5f237ff92ff8a2',
+  },
 ];
 export default merchantList;
